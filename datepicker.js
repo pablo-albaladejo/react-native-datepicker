9f36aa5630f8b674ab8c2633a1da7a4e4a1cbb22
Pablo Albaladejo Mestre <pablo.albaladejo.mestre@gmail.com>
2018-02-23 20:34:31 +0100

    Feature: Suppor for iOS locale

--- datepicker.js
+++ datepicker.js
@@ -30,11 +30,13 @@ class DatePicker extends Component {
 
     this.state = {
       date: this.getDate(),
+      locale: this.getLocale(),
       modalVisible: false,
       animatedHeight: new Animated.Value(0),
       allowPointerEvents: true
     };
 
+    this.getLocale = this.getLocale.bind(this);
     this.getDate = this.getDate.bind(this);
     this.getDateStr = this.getDateStr.bind(this);
     this.datePicked = this.datePicked.bind(this);
@@ -121,6 +123,10 @@ class DatePicker extends Component {
     if (typeof this.props.onCloseModal === 'function') {
       this.props.onCloseModal();
     }
+  }
+
+  getLocale(locale = this.props.locale) {
+    return locale;
   }
 
   getDate(date = this.props.date) {
@@ -392,6 +398,7 @@ class DatePicker extends Component {
                         onDateChange={this.onDateChange}
                         minuteInterval={minuteInterval}
                         timeZoneOffsetInMinutes={timeZoneOffsetInMinutes}
+                        locale={this.state.locale}
                         style={[Style.datePicker, customStyles.datePicker]}
                       />
                     </View>
